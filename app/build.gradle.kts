5ce3e4d4a578e11ab043e0ff9efd97b0ca43f0ef
whambamleo <89803624+whambamleo@users.noreply.github.com>
2024-06-13 22:09:05 -0400

    Updated package names to com.whambamleo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.AppKt"
+    mainClass = "com.whambamleo.AppKt"
 }
 
 tasks.named<Test>("test") {
